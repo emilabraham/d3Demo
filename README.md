92a7780ffb3055747e11e851dd70d53192201a85
Emil Abraham <eabraham@applause.com>
2017-04-05 11:13:45 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Animated Transitions in D3
+
+D3 makes it easy to add meaningful animations to your data visualizations. Whether it’s fading in new items or tweening existing shapes to display new values, adding transitions is easy. This lesson shows you how to add animations while building on your existing knowledge of D3 selections.
+
+### [Lesson Code](http://embed.plnkr.co/github/bclinkinbeard/egghead-d3v4/23-animated-transitions?show=src%2Fapp.js,preview)
+
+### Lesson APIs
+- [d3.select()](https://github.com/d3/d3-selection#select)
+- [selection.transition()](https://github.com/d3/d3-transition#selection_transition)
+- [transition.duration()](https://github.com/d3/d3-transition#transition_duration)
+- [transition.delay()](https://github.com/d3/d3-transition#transition_delay)
+- [transition.ease()](https://github.com/d3/d3-transition#transition_ease)
+- [transition.style()](https://github.com/d3/d3-transition#transition_style)
+- [transition.attr()](https://github.com/d3/d3-transition#transition_attr)
---
+++ src/app.js
@@ -0,0 +1,79 @@
+var margin = { top: 10, right: 20, bottom: 60, left: 80 };
+var height = 400 - margin.top - margin.bottom; //Leave room for axes
+var width = 565 - margin.left - margin.right;
+
+
+var svg = d3.select('.barChart')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    // .call(responsivefy)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+var data = [
+  {score: 63, subject: 'Mathematics'},
+  {score: 82, subject: 'Geography'},
+  {score: 74, subject: 'Spelling'},
+  {score: 97, subject: 'Reading'},
+  {score: 52, subject: 'Science'},
+  {score: 74, subject: 'Chemistry'},
+  {score: 97, subject: 'Physics'},
+  {score: 52, subject: 'ASL'}
+];
+
+var yScale = d3.scaleBand()
+  .domain(data.map(d => d.subject))
+  .range([0, height]);
+  
+var yAxis = d3.axisLeft(yScale);
+svg.call(yAxis);
+
+var xScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+  .ticks(5)
+  .tickSize(10)
+  .tickPadding(5);
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`) //Move down to bottom of svg
+  .call(xAxis);
+
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+    .attr('x', 0)
+    .attr('y', d => yScale(d.subject))
+    .attr('width', d => xScale(d.score))
+    .attr('height', d => yScale.bandwidth());
+
+// function responsivefy(svg) {
+//   // get container + svg aspect ratio
+//   var container = d3.select(svg.node().parentNode),
+//       width = parseInt(svg.style("width")),
+//       height = parseInt(svg.style("height")),
+//       aspect = width / height;
+
+//   // add viewBox and preserveAspectRatio properties,
+//   // and call resize so that svg resizes on inital page load
+//   svg.attr("viewBox", "0 0 " + width + " " + height)
+//       .attr("preserveAspectRatio", "xMinYMid")
+//       .call(resize);
+
+//   // to register multiple listeners for same event type,
+//   // you need to add namespace, i.e., 'click.foo'
+//   // necessary if you call invoke this function for multiple svgs
+//   // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+//   d3.select(window).on("resize." + container.attr("id"), resize);
+
+//   // get width of container and resize svg to fit it
+//   function resize() {
+//       var targetWidth = parseInt(container.style("width"));
+//       svg.attr("width", targetWidth);
+//       svg.attr("height", Math.round(targetWidth / aspect));
+//   }
+// }
